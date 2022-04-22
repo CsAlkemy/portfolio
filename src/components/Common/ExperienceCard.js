19eb5f11fac6f6cb9f92eaab894d982a6f40d8fa
CsAlkemy <alkemy48@gmail.com>
2022-04-22 11:45:56 +0600

    techandexp card details added

--- src/components/Common/ExperienceCard.js
+++ src/components/Common/ExperienceCard.js
@@ -5,7 +5,7 @@ const ExpCard = (props) => {
     return (
         <div className='grid grid-cols-1 md:grid-cols-2 gap-3 px-4 md:px-7 py-3 mx-3 md:mx-0'>
             {props.data.map((index)=>
-            <div key={index.id} className='p-4 bg-black rounded-lg dark:text-white'>
+            <div key={index.id} className='p-4 bg-black rounded-lg dark:text-white relative transform duration-500 hover:-translate-y-1 hover:cursor-pointer'>
                 <div className='flex gap-3'>
                     <div>
                         <img src={index.companyIcon} alt={index.companyName} className="h-16 w-16 object-cover rounded-lg"/>
@@ -16,10 +16,11 @@ const ExpCard = (props) => {
                             <div className={"h-3 w-3 rounded-full my-auto " + (index.endDate ==="present"? "bg-emerald-500":"bg-red-500")}></div>
                             <div className='text-sm font-thin'>{index.position}</div>
                         </div>
+                        <div className='text-xs text-emerald-500'>{index?.joiningDate} to {index?.endDate}</div>
                     </div>
                 </div>
                 <div className='p-3 bg-gray-900 rounded-lg mt-3'>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, sapiente.
+                    {index?.discription}
                 </div>
             </div>
             )}
