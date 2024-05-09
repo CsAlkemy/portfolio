536b36c1334fce6f14665ccdb4f700b4e01d472a
CsAlkemy <alkemy48@gmail.com>
2024-05-09 10:40:29 +0600

    logo added for skills

--- src/components/Common/SkillsCard.js
+++ src/components/Common/SkillsCard.js
@@ -9,6 +9,11 @@ import IconTailwind from '../../images/tailwind.png'
 import IconD3 from '../../images/d3.png'
 import IconGit from '../../images/git-icon.jpg'
 import IconJira from '../../images/jira.png'
+import IconNest from '../../images/nest.svg'
+import IconTs from '../../images/ts.png'
+import IconStory from '../../images/story.png'
+import IconMongo from '../../images/mongo.png'
+import IconNative from '../../images/reactnative.webp'
 
 
 const technicalIcon = () => {
@@ -24,52 +29,79 @@ const technicalIcon = () => {
           icon:IconNext
         },
         {
-          id:3,
+            id:3,
+            name: "Gatsby",
+            icon:IconGatsby
+        },
+        {
+            id:3.5,
+            name: "Native",
+            icon:IconNative
+        },
+        {
+          id:4,
           name: "Vue Js",
           icon:IconVue
         },
         {
-          id:4,
-          name: "Gatsby",
-          icon:IconGatsby
+            id:5,
+            name: "Nest Js",
+            icon:IconNest
         },
         {
-          id:5,
+          id:6,
           name: "Node Js",
           icon:IconNode
         },
         {
-          id:6,
-          name: "Tailwind CSS",
+          id:7,
+          name: "Tailwind",
           icon:IconTailwind
         },
         {
-          id:7,
+            id:8,
+            name: "TypeScript",
+            icon:IconTs
+        },
+        {
+            id:9,
+            name: "Storybook",
+            icon:IconStory
+        },
+        {
+            id:10,
+            name: "Mongo DB",
+            icon:IconMongo
+        },
+        {
+            id:11,
+            name: "Git",
+            icon:IconGit
+        },
+        {
+            id:12,
+            name: "Agile",
+            icon:IconJira
+        },
+        {
+          id:13,
           name: "GraphQl",
           icon:IconGraphQl
         },
         {
-          id:8,
+          id:14,
           name: "D3 js",
           icon:IconD3
-        },
-        {
-          id:9,
-          name: "Version control",
-          icon:IconGit
-        },
-        {
-          id:10,
-          name: "Agila",
-          icon:IconJira
         }
+
       ]
     return (
     <>
-        <div className='grid grid-cols-3 md:grid-cols-5 gap-3 px-4 md:px-7 py-3 mx-3 md:mx-0'>
+        <div className='grid grid-cols-3 md:grid-cols-8 gap-3 px-4 md:px-7 py-3 mx-3 md:mx-0 bg-white dark:bg-black rounded-lg '>
             {technicalData.map((index)=>
-            <div key={index.id} className='p-6 bg-white dark:bg-black rounded-lg border-2 border-emerald-500 relative transform duration-500 hover:-translate-y-1 hover:cursor-pointer hover:bg-gray-900'>
-                <img src={index.icon} alt={index.name} className={index.name === 'Tailwind CSS' ? 'rounded-full object-cover':'object-cover'}/>
+            <div key={index.id} className='p-2 rounded-lg flex flex-col justify-center items-center relative transform duration-500 hover:-translate-y-1 hover:cursor-pointer hover:bg-gray-900'>
+                <img src={index.icon} alt={index.name} className={`${index.name === 'Tailwind CSS' ? 'rounded-full object-cover':'object-cover'}`}/>
+                <div className='text-white text-[10px] mt-2'>{index.name}</div>
             </div>
             )}
         </div>
